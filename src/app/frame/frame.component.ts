07fed2e04bc141dba0f0e31feff2da3ff594a1c1
Chris Engelsma <chris.engelsma@gmail.com>
2021-04-12 13:45:32 -0500

    Resizing

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,12 +1,25 @@
 import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+export type ResizeAnchorType =
+  | 'top'
+  | 'left'
+  | 'bottom'
+  | 'right'
+
+export type ResizeDirectionType =
+  | 'x'
+  | 'y'
+  | 'xy';
+
 @Component({
   selector: 'app-frame',
   templateUrl: './frame.component.html',
   styleUrls: [ './frame.component.scss' ]
 })
 export class FrameComponent {
+
+  @ViewChild('resizeCorner') resizeCornerRef!: ElementRef;
 
   @ViewChild('wrapper') wrapperRef!: ElementRef;
 
@@ -48,5 +61,59 @@ export class FrameComponent {
 
     this._document.addEventListener('mousemove', duringDrag);
     this._document.addEventListener('mouseup', finishDrag);
+  }
+
+  startResize($event, anchors: ResizeAnchorType[], direction: ResizeDirectionType): void {
+    $event.preventDefault();
+    const mouseX = $event.clientX;
+    const mouseY = $event.clientY;
+    const lastX = this.position.x;
+    const lastY = this.position.y;
+    const dimensionWidth = this.resizeCornerRef.nativeElement.parentNode.offsetWidth;
+    const dimensionHeight = this.resizeCornerRef.nativeElement.parentNode.offsetHeight;
+
+    const duringResize = (e) => {
+      let dw = dimensionWidth;
+      let dh = dimensionHeight;
+      if (direction === 'x' || direction === 'xy') {
+        if (anchors.includes('left')) {
+          dw += ( mouseX - e.clientX );
+        } else if (anchors.includes('right')) {
+          dw -= ( mouseX - e.clientX );
+        }
+      }
+      if (direction === 'y' || direction === 'xy') {
+        if (anchors.includes('top')) {
+          dh += ( mouseY - e.clientY );
+        } else if (anchors.includes('bottom')) {
+          dh -= ( mouseY - e.clientY );
+        }
+      }
+
+      if (anchors.includes('left')) {
+        this.position.x = lastX + e.clientX - mouseX;
+        this.size.w = Math.max(dw, this.minSize.w);
+      }
+
+      if (anchors.includes('top')) {
+        this.position.y = lastY + e.clientY - mouseY;
+        this.size.h = Math.max(dh, this.minSize.h);
+      }
+
+      if (anchors.includes('bottom') || anchors.includes('right')) {
+        this.size.w = Math.max(dw, this.minSize.w);
+        this.size.h = Math.max(dh, this.minSize.h);
+      }
+
+      this.lastSize = { ...this.size };
+    };
+
+    const finishResize = (e) => {
+      this._document.removeEventListener('mousemove', duringResize);
+      this._document.removeEventListener('mouseup', finishResize);
+    };
+
+    this._document.addEventListener('mousemove', duringResize);
+    this._document.addEventListener('mouseup', finishResize);
   }
 }
